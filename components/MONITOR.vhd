c7997ca15aeda1e3fb3e33f87f2cf4d742e2da66
Leomar Marcelo Marschalk <leomarmm1@gmail.com>
2022-08-18 19:02:59 -0300

    Adaptações no código para o monitor.

--- components/MONITOR.vhd
+++ components/MONITOR.vhd
@@ -1,27 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-entity TOP_UART is
+entity MONITOR is
+generic
+(
+	baud				:	integer := 9600;
+	clock				:	integer := 50000000;
+	input_bytes		:	integer := 3;
+	output_bytes	:	integer := 3
+);
 port
 (
 	i_RX		:	in std_logic;
 	i_CLK		:	in std_logic;
 	i_RST		:	in std_logic;
-	o_TX		:	out std_logic
+	o_TX		:	out std_logic;
+	o_BYTES	:	out std_logic_vector(8*input_bytes-1 downto 0)
 );
-end TOP_UART;
+end MONITOR;
 
-architecture behavioral of TOP_UART is
+architecture behavioral of MONITOR is
 
-	type top_state is (IDLE, RECV, LOAD, SEND_INIT, SEND);
+	type top_state is (IDLE, RECV, SAVE);
 	attribute syn_encoding : string;
-	attribute syn_encoding of top_state : type is "safe";
+	attribute syn_encoding of top_state :  type is "safe";
 
 	component UART_RX
 	generic
 	(
-		baud		:	integer	:= 9600;
-		clock		:	integer	:=	50000000
+		baud	:	integer	:= baud;
+		clock	:	integer	:=	clock
 	);
 	port
 	(
@@ -36,8 +44,8 @@ architecture behavioral of TOP_UART is
 	component UART_TX is
 	generic
 	(
-		baud			:	integer	:= 9600;
-		clock			:	integer	:= 50000000
+		baud	:	integer	:= baud;
+		clock	:	integer	:= clock
 	);
 	port
 	(
@@ -55,7 +63,9 @@ architecture behavioral of TOP_UART is
 	signal w_LS			:	std_logic;
 	signal w_RTS		:	std_logic;
 	signal w_RECV		:	std_logic;
+	signal w_VRST		:	std_logic;
 	signal t_STATE		:	top_state;
+	signal r_RX			:	std_logic_vector(8*input_bytes-1 downto 0);
 
 begin
 
@@ -75,24 +85,21 @@ begin
 		i_DATA	=>	w_DATA_TX,
 		i_CLK		=>	i_CLK,
 		i_RST		=>	i_RST,
-		i_LS		=>	w_LS,
+		i_LS		=>	'1',
 		o_TX		=>	o_TX,
 		o_RTS		=>	w_RTS
 	);
 	
 	w_DATA_TX <= w_DATA_RX + '1';
-	
-	with t_STATE select w_LS <=
-		'1' when IDLE,
-		'1' when RECV,
-		'1' when LOAD,
-		'0' when SEND_INIT,
-		'0' when SEND;
+	o_BYTES <= r_RX;
 
 	process(i_CLK, i_RST, t_STATE)
+		variable v_RX : std_logic_vector(8*input_bytes downto 0);
 	begin
 		if(i_RST = '1') then
 			t_STATE <= IDLE;
+			v_RX := (0 => '1', OTHERS => '0');
+			r_RX <= (OTHERS => '0');
 		elsif(falling_edge(i_CLK)) then
 			case t_STATE is
 				when IDLE =>
@@ -103,23 +110,18 @@ begin
 					end if;
 				when RECV =>
 					if(w_RECV = '0') then
-						t_STATE <= LOAD;
+						t_STATE <= SAVE;
 					else
 						t_STATE <= RECV;
 					end if;
-				when LOAD =>
-					t_STATE <= SEND_INIT;
-				when SEND_INIT =>
-					if(w_RTS = '0') then
-						t_STATE <= SEND;
-					else
-						t_STATE <= SEND_INIT;
-					end if;
-				when SEND =>
-					if(w_RTS = '1') then
+				when SAVE =>
+					v_RX := v_RX(8*(input_bytes-1) downto 0) & w_DATA_RX;
+					if(v_RX(v_RX'high) = '1') then
+						r_RX <= v_RX(8*input_bytes-1 downto 0);
+						v_RX := (0 => '1', OTHERS => '0');
 						t_STATE <= IDLE;
 					else
-						t_STATE <= SEND;
+						t_STATE <= IDLE;
 					end if;
 			end case;
 		end if;
--- testbenches/TB_MONITOR.vhd
+++ testbenches/TB_MONITOR.vhd
@@ -2,39 +2,52 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity TB_TOP_UART is
+entity TB_MONITOR is
 generic
 (
-	baud			:	integer	:=	9600
+	baud				:	integer := 9600;
+	input_bytes		:	integer := 3;
+	output_bytes	:	integer := 3
 );
-end TB_TOP_UART;
+end TB_MONITOR;
 
-architecture testbench of TB_TOP_UART is
+architecture testbench of TB_MONITOR is
 
 	constant delay	:	time	:= 1 sec*(1.0/real(baud));
-	component TOP_UART
+	component MONITOR
+	generic
+	(
+		baud				:	integer := baud;
+		clock				:	integer := 50000000;
+		input_bytes		:	integer := input_bytes;
+		output_bytes	:	integer := output_bytes
+	);
 	port
 	(
 		i_RX		:	in std_logic;
 		i_CLK		:	in std_logic;
 		i_RST		:	in std_logic;
-		o_TX		:	out std_logic
+		o_TX		:	out std_logic;
+		o_BYTES	:	out std_logic_vector(8*input_bytes-1 downto 0)
 	);
 	end component;
+
 	signal w_RX		:	std_logic;
 	signal w_CLK	:	std_logic;
 	signal w_RST	:	std_logic;
 	signal w_TX		:	std_logic;
-
+	signal w_BYTES	:	std_logic_vector(8*input_bytes-1 downto 0);
+	
 begin
 
-	U1 : TOP_UART
+	U1 : MONITOR
 	port map
 	(
 		i_RX	=> w_RX,
 		i_CLK	=> w_CLK,
 		i_RST	=> w_RST,
-		o_TX	=> w_TX
+		o_TX	=> w_TX,
+		o_BYTES => w_BYTES
 	);
 
 	-- Clock
