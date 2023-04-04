cf742b6c58c9ba96ea4f82ca34a8b6270bff7581
Leomar Marcelo Marschalk <leomarmm1@gmail.com>
2023-04-04 00:08:51 -0300

    Início do back-end da aplicação. Criação de módulo CRC8 para Javascript.

---
+++ Application/backend/crc8/crc8.js
@@ -0,0 +1,36 @@
+module.exports = class crc8
+{
+    constructor(polynomial)
+    {
+        this.poly = polynomial
+    }
+    
+    calculateCRC(bytearray)
+    {
+        var crc = 0;
+        bytearray.forEach((_byte) => 
+        {
+            for (var bit = 7; bit >= 0; bit--)
+            {
+                var crc_7 = (crc >> 7) & 1;
+                var newCRC = crc_7 ^ ((_byte >> bit) & 1);
+                
+                for (var i = 1; i <= 7; i++)
+                {
+                    var poly_i = (this.poly >> i) & 1;
+                    var crc_i = (crc >> (i-1)) & 1;
+                    if(poly_i)
+                    {
+                        newCRC |= (crc_7 ^ crc_i) << i;
+                    }
+                    else
+                    {
+                        newCRC |= (crc_i) << i;
+                    }
+                }
+                crc = newCRC;
+            }
+        });
+        return crc;
+    }
+}
